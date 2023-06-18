bb7269726b1ad5cc14c6e3b367a84913ae77ea4b
Axel <axelchavezj@gmail.com>
2023-06-17 18:19:01 -0600

    First commit of the project, creation of the html, js and css files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Access the heading element
+var heading = document.querySelector('h1');
+
+// Change the text when clicked
+heading.addEventListener('click', function() {
+  heading.textContent = 'Clicked!';
+});
